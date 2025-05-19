51eccde19a4a81621843494f8ea916e47d336411
velychkov <skyiner@gmail.com>
2025-05-20 01:22:04 +0300

    change provider auth logic

--- terraform/apps/provider.tf
+++ terraform/apps/provider.tf
@@ -27,13 +27,21 @@ provider "aws" {
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this.name]
+  }
 }
 
 provider "helm" {
   kubernetes {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.this.name]
+  }
   }
 }
